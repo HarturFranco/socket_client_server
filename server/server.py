c37ce85bbd8116f966aac326103130bff93e0bc1
HarturFranco <franco.s.arthur00@gmail.com>
2022-03-04 17:48:50 -0300

    server v1

---
+++ server/server.py
@@ -0,0 +1,19 @@
+import socket
+from socketserver import UDPServer
+
+ip = "192.168.1.13"
+port = "2022"
+
+bufferSize = 1024
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+orign = (ip, port)
+udp.bind(orign)
+
+while(True):
+    message, address = udp.recvfrom(bufferSize)
+    
+    print(f'Client Message: {message}\nClient IP: {address}')
+    
+    # Manda para o cliente a mensagem recebida
+    udp.sendto(str.encode(message), address)
